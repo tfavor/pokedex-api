f6a64c61056556bb7002bcbace4127901671acc8
Tristan Favor <tristanfavor29@gmail.com>
2019-10-14 15:30:46 -0500

    getting ready for production

--- server.js
+++ server.js
@@ -1,16 +1,68 @@
 const express = require('express');
 const morgan = require('morgan');
+const dotenv = require('dotenv').config();
+const POKEDEX = require('./pokedex.json');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+app.use(helmet())
+app.use(cors())
 
-app.use((req, res) => {
-    res.send('hello world')
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');    
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        res.status(401).json({ error: 'Unauthorized request'});
+    }
+    next()
 })
 
-const PORT = 8000
+function handleGetTypes(req, res) {
+    res.json(validTypes)
+}
 
-app.listen(PORT, () => {
-  console.log(`server listening at http://localhost:${PORT}`)
+app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
+    let response = POKEDEX.pokemon;
+
+  // filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    )
+  }
+
+  // filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    )
+  }
+
+  res.json(response)
+}
+
+app.use((error, req, res, next) => {
+  let response 
+  if(process.env.NODE_ENV === 'productio') {
+    response = { error: 'server error' }
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
 })
+
+app.get('/pokemon', handleGetPokemon)
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {})
